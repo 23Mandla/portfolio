3550eaa537b510fe1e9988a46b93a09fef305201
Mandla <mandlambolekwa@gmail.com>
2024-01-11 16:13:25 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+#  This is a redesign of profile
+
+I will using using react for -
+front end-end,
+
+
+node js and espress for backend
+
+
+mongoDB for my database
---
+++ src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="fixed top-0 left-0 bg-[rgba(0, 0, 0, 0.8)] h-full w-full flex item-center z-10">
+      <h1>Portfolio</h1>
+    </div>
+  );
+}
+
+export default App;
